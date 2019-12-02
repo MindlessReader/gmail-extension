fad351344f8d1856750a7958cdfc08182d1f6b5f
MindlessReader <fabian@thevks.com>
2019-12-01 21:15:18 -0800

    Began work on spam detection algorithm!

--- test.js
+++ test.js
@@ -20,6 +20,75 @@
 //
 ////class gb_hb
 ////(' + modifyDOM + ')();
-var email = document.getElementsByClassName("gb_kb");
 
-alert(email[0]);
+function searchString(stringToSearch, searchTerm) {
+    var re = new RegExp(searchTerm, 'g');
+    return (stringToSearch.match(re));
+}
+
+setTimeout(function() {
+    var spamScore = 1;
+    var blacklistedEmail = false;
+    var counter = 0;
+    var email = document.getElementsByClassName("gb_ob")[0].innerHTML;
+    var body = document.getElementsByClassName("ii gt")[0].innerHTML;
+    var incEmail = document.getElementsByClassName("go")[0].textContent;
+    var blacklistedEmails = ["1000059152@tvusd.us"];
+    var suspiciousWordsCom = ["communism", "communist", "communists", "communistic"]; //Communism
+    var suspiciousWordsRom = ["communism", "communist", "communists", "communistic"]; //Romantic
+    var suspiciousWordsExt = ["password", "malware", "vulnerability", "critical vulnerability",
+        "trojan", "btc", "adult sites", "Bitcoins", "BTC wallet", "virus", "RAT",
+        "http://www.login.blockchain.com/en/#/signup/", "http://www.paxful.com/", "http://www.coingate.com/",
+        "http://www.coinbase.com/", "Drive-by exploit", "send the video to all your contacts",
+        "after receiving the payment", "control over your computer", "see everything", "full access", "darknet",
+        "darkweb", "post it on social network"
+    ]; //Extortion
+    var commonWords = [];
+
+    commonWords.forEach(element => {
+        var re = new RegExp(element, 'g');
+        body = body.replace(re, '');
+    });
+
+    console.log(body);
+
+    incEmail = incEmail.slice(1, incEmail.length - 1);
+    blacklistedEmails.push(email);
+
+    suspiciousWordsCom.forEach(element => {
+        if (searchString(body, element) != null && searchString(body, element) != undefined) {
+            spamScore = spamScore * (searchString(body, element).length * 2);
+        }
+    });
+
+    suspiciousWordsRom.forEach(element => {
+        if (searchString(body, element) != null && searchString(body, element) != undefined) {
+            spamScore = spamScore * (searchString(body, element).length * 2);
+        }
+    });
+
+    suspiciousWordsExt.forEach(element => {
+        if (searchString(body, element) != null && searchString(body, element) != undefined) {
+            spamScore = spamScore * (searchString(body, element).length * 2);
+        }
+    });
+
+    blacklistedEmails.forEach(element => {
+        if (element == incEmail) {
+            blacklistedEmail = true;
+        }
+    });
+
+    console.log(spamScore);
+    if (blacklistedEmail == true) {
+        alert("We detected that this email came from a blacklisted source, either a known spam address, or someone impersonating your own email address.\nSpam Score: " + spamScore);
+    } else if (spamScore >= 1000) {
+        alert("This email is likely spam!\nSpam Score: " + spamScore);
+    }
+
+    /* 
+        alert("User Email: " + email);
+        alert("Body: " + body);
+        alert("Incoming Email Address: " + incEmail);
+     */
+}, 500);
