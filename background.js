43bb71efe1e1c414efed3be9c889d9d83410116f
MindlessReader <fabian@thevks.com>
2019-12-13 14:29:41 -0800

    Finished cleaning code

--- background.js
+++ background.js
@@ -25,7 +25,7 @@ chrome.runtime.onInstalled.addListener(function() {
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     // read changeInfo data and do something with it (like read the url)
-    if (changeInfo.url != "https://mail.google.com/mail/u/0/#inbox" && changeInfo.url != "https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox" && changeInfo.url) {
+    if (changeInfo.url != "https://mail.google.com/mail/u/0/#inbox" && changeInfo.url != "https://mail.google.com/mail/u/0/?tab=rm0&ogbl#inbox" && changeInfo.url) {
         console.log("Not Inbox");
         chrome.tabs.executeScript({
             file: 'test.js'
